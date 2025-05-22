7c398fd6f3d0e9fff36f3de0d8117f0d59414f13
Hemaraju S <95607299+Hemaraju-S16@users.noreply.github.com>
2025-05-22 15:24:55 +0530

    added code

---
+++ __init__.py
@@ -0,0 +1,9 @@
+bl_info = {
+    "name": "AvatarAnalyzer",
+    "author": "hemaraju_gowda",
+    "version": (1, 0),
+    "blender": (4, 3, 0),
+    "location": "View3D > Sidebar > MyAddon Tab",
+    "description": "Displays the angle between two selected bones",
+    "category": "mesh",
+}
